d07a7c92a7be505399fbca90b4e99defaff6df19
ryo katsuma <katsuma@gmail.com>
2015-05-25 00:38:01 +0900

    Use customView, ChannelContainerView

---
+++ Remocon/ChannelContainerView.swift
@@ -0,0 +1,55 @@
+//
+//  ChannelContainerView.swift
+//  Remocon
+//
+//  Created by Ryo Katsuma on 2015/05/24.
+//  Copyright (c) 2015年 Ryo Katsuma. All rights reserved.
+//
+
+import UIKit
+
+class ChannelContainerView: UIView {
+    lazy private var buttons: [ChannelButton] = self.createChannelButtons()
+
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layoutChannelButtons()
+    }
+
+    private func commonInit() {
+        for var i = 0; i < buttons.count; i++ {
+            buttons[i].label = ConfigurationService.buttons[i]["label"]
+            buttons[i].channel = ConfigurationService.buttons[i]["channel"]
+            self.addSubview(buttons[i])
+        }
+    }
+
+    private func createChannelButtons() -> [ChannelButton] {
+        var buttons: [ChannelButton] = []
+
+        for var i = 0; i < ConfigurationService.buttons.count; i++ {
+            buttons.append(ChannelButton(frame: CGRectZero))
+        }
+        return buttons
+
+    }
+
+    private func layoutChannelButtons() {
+        for var i = 0; i < buttons.count; i++ {
+            buttons[i].frame.size = CGSizeMake(80, 80)
+            buttons[i].frame.origin.x = CGFloat(85 * (i % 3))
+            buttons[i].frame.origin.y = CGFloat(85 * (i / 3))
+        }
+    }
+
+}
--- Remocon/ViewController.swift
+++ Remocon/ViewController.swift
@@ -11,46 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: - Properties -
-    lazy private var buttons: [ChannelButton] = self.createChannelButtons()
+    lazy private var channelContainerView: ChannelContainerView = self.createChannelContainerView()
 
     // MARK: - Life cycle events -
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        for var i = 0; i < buttons.count; i++ {
-            buttons[i].label = ConfigurationService.buttons[i]["label"]
-            buttons[i].channel = ConfigurationService.buttons[i]["channel"]
-            self.view.addSubview(buttons[i])
-        }
+        self.view.addSubview(channelContainerView)
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        self.layoutChannelButtons()
+        self.layoutChannelContainerView()
     }
 
     // MARK: - Create subviews -
-    private func createChannelButtons() -> [ChannelButton] {
-        var buttons: [ChannelButton] = []
-
-        for var i = 0; i < ConfigurationService.buttons.count; i++ {
-            var x: CGFloat = CGFloat(85 * (i % 3))
-            var y: CGFloat = CGFloat(85 * (i / 3))
-            buttons.append(ChannelButton(frame: CGRectMake(x, y, 80, 80)))
-        }
-        return buttons
+    private func createChannelContainerView() -> ChannelContainerView {
+        return ChannelContainerView(frame: CGRectZero)
     }
 
     // MARK: - Layout subviews -
-    private func layoutChannelButtons() {
-        for var i = 0; i < buttons.count; i++ {
-            var button: UIButton = buttons[i]
-            button.addTarget(self, action: "onClickButton:", forControlEvents: .TouchUpInside)
-        }
-    }
-
-    func onClickButton(sender: UIButton) {
-        println("clicked \(sender.tag)")
+    private func layoutChannelContainerView() {
+        channelContainerView.frame.size = CGSizeMake(250, 335)
+        channelContainerView.center = self.view.center
     }
 }
 
